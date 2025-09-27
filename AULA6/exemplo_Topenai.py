1e59af9f5619e9605c8f68ea4a69511c434978a7
aneli-hub <oliveiraaninha587@gmail.com>
2025-09-27 15:17:36 -0300

    Update exemplo_Topenai.py

--- AULA6/exemplo_Topenai.py
+++ AULA6/exemplo_Topenai.py
@@ -2,7 +2,7 @@
 from openai import OpenAI # biblioteca do chatgpt
 
 # inicializar o cliente 
-cliente = OpenAI(api_key="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")
+cliente = OpenAI(api_key="coloque a chave")
 
 # https://abre.ai/chaveopenai (link da chave de API, fornecida pelo professor)
 # OpenAi dashboard -> conta de vocês (para solicitar nossas proprias chaves)
